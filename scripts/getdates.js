6728a9d7864fa2a90b8d71cc933b2f9d9cf5586d
Arcee Juan <arceetaraguajuan@gmail.com>
2024-04-23 18:35:40 +0800

    updated html and js

--- scripts/getdates.js
+++ scripts/getdates.js
@@ -1,24 +1,23 @@
 document.addEventListener("DOMContentLoaded", function () {
-    // select the DOM elements for output
-    const currentYearElement = document.querySelector("#currentyear");
-    const lastModifiedElement = document.querySelector("#lastModified");
-  
-    // get the current year
-    const currentYear = new Date().getFullYear();
-  
-    // set the copyright year dynamically
-    currentYearElement.textContent = currentYear;
-  
-    // get the last modified date dynamically
-    const lastModifiedDate = new Date(document.lastModified);
-  
-    // set the last modified date dynamically
-    lastModifiedElement.textContent = `Last modified: ${new Intl.DateTimeFormat(
+  // select the DOM elements for output
+  const currentYearElement = document.querySelector("#currentYear");
+  const lastModifiedElement = document.querySelector("#lastModifiedDate");
+
+  // get the current year
+  const currentYear = new Date().getFullYear();
+
+  // set the copyright year dynamically
+  currentYearElement.textContent = currentYear;
+
+  // get the last modified date dynamically
+  const lastModifiedDate = new Date(document.lastModified);
+
+  // set the last modified date dynamically
+  lastModifiedElement.textContent = `Last modified: ${new Intl.DateTimeFormat(
       "en-US",
       {
-        dateStyle: "medium",
-        timeStyle: "short",
+          dateStyle: "medium",
+          timeStyle: "short",
       }
-    ).format(lastModifiedDate)}`;
-  });
-  
+  ).format(lastModifiedDate)}`;
+});
